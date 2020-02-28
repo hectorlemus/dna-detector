03e8ca6a83e389d08848bcc87c35ba36b4075993
hectorlemus <hero.lemus.719@gmail.com>
2020-02-28 13:31:48 -0600

    init

---
+++ src/main/java/dev/demo/dnadetector/DnaDetectorApplication.java
@@ -0,0 +1,13 @@
+package dev.demo.dnadetector;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DnaDetectorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DnaDetectorApplication.class, args);
+	}
+
+}
