7f95c04b8c7464a7b829e4647f0d2c745eb3d042
hectorlemus <hero.lemus.719@gmail.com>
2020-02-29 17:06:27 -0600

    Se agrega servicio verificacion de adn

---
+++ src/main/java/dev/demo/dnadetector/service/DNAService.java
@@ -0,0 +1,17 @@
+package dev.demo.dnadetector.service;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class DNAService {
+
+    public String [] createDNA() {
+        String[] dna = {};
+        return dna;
+    }
+
+    public Boolean hasMutation(final String [] dna) {
+        return true;
+    }
+
+}
