6e4f222c500616b86583ec851d6cb0b92446add4
hectorlemus <hero.lemus.719@gmail.com>
2020-03-03 18:22:23 -0600

    Se agrega clase para manejar peticion de validacion de ADN

---
+++ src/main/java/dev/demo/dnadetector/entity/DNARequest.java
@@ -0,0 +1,14 @@
+package dev.demo.dnadetector.entity;
+
+public class DNARequest {
+
+    private String[] dna;
+
+    public String[] getDna() {
+        return dna;
+    }
+
+    public void setDna(String[] dna) {
+        this.dna = dna;
+    }
+}
