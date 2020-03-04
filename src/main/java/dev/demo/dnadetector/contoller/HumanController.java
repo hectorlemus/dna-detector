2d6b204f5d42b84ad7dd1e90ba8e3bd6353117da
hectorlemus <hero.lemus.719@gmail.com>
2020-03-03 18:39:52 -0600

    Se agrega controlador con endpoints para crear humanos, obtener humanos mutantes, obtener humanos no mutantes.

--- src/main/java/dev/demo/dnadetector/contoller/HumanController.java
+++ src/main/java/dev/demo/dnadetector/contoller/HumanController.java
@@ -1,7 +1,10 @@
 package dev.demo.dnadetector.contoller;
 
+import dev.demo.dnadetector.entity.DNA;
 import dev.demo.dnadetector.entity.Human;
-import dev.demo.dnadetector.service.HumanService;
+import dev.demo.dnadetector.repository.DNARepository;
+import dev.demo.dnadetector.repository.HumanRepository;
+import dev.demo.dnadetector.service.DNAService;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -12,38 +15,50 @@ import java.util.ArrayList;
 @RequestMapping(path = "api/")
 public class HumanController {
 
-    private final HumanService humanService;
+    private final DNAService dnaService;
+    private final HumanRepository humanRepository;
+    private final DNARepository dnaRepository;
 
-    public HumanController(HumanService humanService) {
-        this.humanService = humanService;
+    public HumanController(
+        final HumanRepository humanRepository,
+        final DNAService dnaService,
+        final DNARepository dnaRepository
+    ) {
+        this.humanRepository = humanRepository;
+        this.dnaService = dnaService;
+        this.dnaRepository = dnaRepository;
     }
 
     @PostMapping(path = "/human")
     public @ResponseBody ResponseEntity<Human> createHuman (@RequestBody Human human) {
         try {
-            humanService.validateMutation(human);
-            return new ResponseEntity<>(human, HttpStatus.CREATED);
+
+            DNA dna = dnaService.createRandomDNA();
+            DNA dnaDB = dnaRepository.save(dna);
+
+            human.setDna(dnaDB);
+            Human humanDB = humanRepository.save(human);
+
+            return new ResponseEntity<>(humanDB, HttpStatus.CREATED);
         } catch (Exception ex) {
             return new ResponseEntity<>(HttpStatus.INTERNAL_SERVER_ERROR);
         }
     }
 
     @GetMapping(path = "/mutant-humans")
-    @CrossOrigin(origins = "*")
     public @ResponseBody
     Iterable<Human> getMutantHumans() {
-        try {
-            return humanService.getHumans(true);
-        } catch (Exception ex) {
-            return new ArrayList<>();
-        }
+        return getHumans(true);
     }
 
     @GetMapping(path = "/non-mutant-humans")
-    @CrossOrigin(origins = "*")
     public @ResponseBody Iterable<Human> getNonMutantHumans() {
+        return getHumans(false);
+    }
+
+    private Iterable<Human> getHumans(final Boolean mutants) {
         try {
-            return humanService.getHumans(false);
+            return humanRepository.findByDnaMutation(mutants);
         } catch (Exception ex) {
             return new ArrayList<>();
         }
